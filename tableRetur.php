dbab4a6ac726326fe920395c8a4d65fd6caa1451
Ferdinandus-Nicholas <78593233+Ferdinandus-Nicholas@users.noreply.github.com>
2021-12-27 15:06:37 +0700

    fix all

--- tableRetur.php
+++ tableRetur.php
@@ -130,7 +130,7 @@ $jum = count($arr1);
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="#!">Laporan</a></li>
+                        <li><?php print"<a class='dropdown-item' target = '_blank' href='tableLaporan.php?username=".$user."'>Laporan</a>" ?></li>
                         <li><hr class="dropdown-divider" /></li>
                         <li><a class="dropdown-item" href="login.php">Logout</a></li>
                     </ul>
--- tables.php
+++ tables.php
@@ -135,7 +135,7 @@ if ($jum > 0) {
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
                 <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                    <li><a class="dropdown-item" href="#!">Laporan</a></li>
+                    <li><?php print"<a class='dropdown-item' target = '_blank' href='tableLaporan.php?username=".$user."'>Laporan</a>" ?></li>
                     <li>
                         <hr class="dropdown-divider" />
                     </li>
